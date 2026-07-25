79b7e20c2de34df58262054f04a55fdf1098e65b
vladimir castro feliz <vladimicastro@gmail.com>
2026-07-25 00:32:27 -0400

    Agregada ruta para crear usuarios

--- src/index.js
+++ src/index.js
@@ -3,19 +3,41 @@ const pool = require('./db');
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('API funcionando correctamente');
 });
 
+// Mostrar usuarios
 app.get('/usuarios', async (req, res) => {
     try {
         const resultado = await pool.query('SELECT * FROM usuarios');
         res.json(resultado.rows);
+
     } catch (error) {
         console.log(error);
         res.status(500).send('Error al obtener los usuarios');
+    }
+});
+
+// Crear usuario
+app.post('/usuarios', async (req, res) => {
+    try {
+        const { nombre, correo, edad } = req.body;
+
+        const resultado = await pool.query(
+            'INSERT INTO usuarios (nombre, correo, edad) VALUES ($1, $2, $3) RETURNING *',
+            [nombre, correo, edad]
+        );
+
+        res.json(resultado.rows[0]);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error al crear usuario');
     }
 });
 
